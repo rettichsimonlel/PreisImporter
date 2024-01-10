a8a639b1828899945b707f8ef0a8818a09dfc9cc
rettichsimonlel <retter190085@sr.htlweiz.at>
2024-01-10 08:35:01 +0100

    Adding more data to see if it works

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import matplotlib.pyplot as plt
 
-print("Hello world")
-
 def import_data(filename):
     lines = []
     with open(filename, 'r') as f:
@@ -15,28 +13,22 @@ def import_data(filename):
 
 
 def visualize_data(data):
-    # Create a dictionary to store data for each company in each country
     company_data = {}
 
-    # Iterate over each entry in the data
     for entry in data:
         company = entry[0]
-        country = entry[4]  # Assuming country information is in the fifth position [4]
-        time = entry[1]  # Assuming time information is in the second position [1]
+        country = entry[4]
+        time = entry[1]
         stock_price = entry[2]
 
-        # Create a unique key for each company in each country
         key = f"{company}_{country}"
 
-        # If the key is not in the dictionary, create a new entry
         if key not in company_data:
             company_data[key] = {"times": [], "prices": []}
 
-        # Append time and stock price to the respective lists
         company_data[key]["times"].append(time)
         company_data[key]["prices"].append(stock_price)
 
-    # Create a separate plot for each company in each country
     for key, values in company_data.items():
         company, country = key.split("_")
         times = values["times"]
@@ -51,3 +43,10 @@ def visualize_data(data):
         plt.show()
 
     return True
+
+def main():
+    data = import_data('data.txt')
+    visualize_data(data)
+
+if __name__ == "__main__":
+    main()
